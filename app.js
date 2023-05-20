b1c46100a7041a3bfec0fd139d6e28831171cb58
EvanilsonP <pevanilson99@gmail.com>
2023-05-20 17:11:42 -0300

    Connecting to mongoDB Atlas

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const db = require('./database/db.js');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+db.database();
 
 app.listen(PORT, () => console.log(`${PORT}`));
