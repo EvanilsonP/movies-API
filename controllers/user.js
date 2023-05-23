c4d6fdf24fff79827c67b379b45d104df23e371c
EvanilsonP <pevanilson99@gmail.com>
2023-05-23 13:01:54 -0300

    Creating signup routes

---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+
+const signup = (req, res) => {
+    res.send('Sign up')
+};
+
+module.exports = {signup};
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 const controllers = require('../controllers/movie');
+const userController = require('../controllers/user');
 
 routes.get('/', controllers.welcome);
 routes.post('/', controllers.createMovie);
 routes.get('/movies', controllers.listAllMovies);
 routes.put('/movie/:id', controllers.updateMovie);
 routes.delete('/movie/:id', controllers.deleteMovie);
+
+routes.get('/signup', userController.signup);
 
 module.exports = routes;
